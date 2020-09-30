6efe9fae21b9d222b2b9cace4cf640fa04ee7578
muradshahsuvarov <42478293+muradshahsuvarov@users.noreply.github.com>
2020-09-30 15:25:03 +0400

    Final version

--- app/src/main/java/com/example/minisweaperiqultev2/MainActivity.kt
+++ app/src/main/java/com/example/minisweaperiqultev2/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.minisweaperiqultev2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.minisweaperiqulte.custom_adapter
 import com.example.minisweaperiqulte.customlayout
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var flagIsAllowed = false
+        var gameDone : Boolean = false
+        var restartButton : Button? = null
+        var choiceButton : Button? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +26,30 @@ class MainActivity : AppCompatActivity() {
         var adapter = custom_adapter(this, R.layout.customlayout, data)
 
         gridView.adapter = adapter
+
+        restartButton = findViewById(R.id.restartBtn)
+        restartBtn.setBackgroundColor(Color.GREEN)
+
+        choiceButton = findViewById(R.id.choiceBtn)
+        choiceBtn.setBackgroundColor(Color.YELLOW)
+
+        restartBtn.setOnClickListener {
+            adapter = custom_adapter(this, R.layout.customlayout, data)
+            gridView.adapter = adapter
+            restartBtn.setBackgroundColor(Color.GREEN)
+            choiceBtn.setBackgroundColor(Color.YELLOW)
+            gameDone = false
+        }
+
+        choiceBtn.setOnClickListener {
+            if(flagIsAllowed == false){
+                choiceBtn.text = "Put a flag"
+                flagIsAllowed = true
+            }else{
+                choiceBtn.text = "Try a field"
+                flagIsAllowed = false
+            }
+        }
 
     }
 
@@ -35,18 +69,20 @@ class MainActivity : AppCompatActivity() {
                         if(mineCount != 0){
                             var randNumber : Int = Random.nextInt(0,5)
                             if(randNumber == 3){
-                                item_list.add(customlayout(R.mipmap.ic_launcher,Cell(j,i,true,0,false,false)))
+                                item_list.add(customlayout(R.drawable.box,Cell(j,i,true,0,false,false)))
                                 mineCount--
                             }else{
-                                item_list.add(customlayout(R.mipmap.ic_launcher,Cell(j,i,false,0,false,false)))
+                                item_list.add(customlayout(R.drawable.box,Cell(j,i,false,0,false,false)))
                             }
                         }else{
-                            item_list.add(customlayout(R.mipmap.ic_launcher,Cell(j,i,false,0,false,false)))
+                            item_list.add(customlayout(R.drawable.box,Cell(j,i,false,0,false,false)))
                             stopGeneratingMines = true
                         }
 
                     }
                 }
+
+
 
             return item_list
         }
--- app/src/main/java/com/example/minisweaperiqultev2/custom_adapter.kt
+++ app/src/main/java/com/example/minisweaperiqultev2/custom_adapter.kt
@@ -2,13 +2,17 @@ package com.example.minisweaperiqulte
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.media.Image
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageView
+import com.example.minisweaperiqultev2.MainActivity
 import com.example.minisweaperiqultev2.R
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.customlayout.view.*
 
@@ -16,18 +20,135 @@ class custom_adapter(private val getContext : Context, private val CustomLayoutI
  private val custom_item : ArrayList<customlayout>)
     : ArrayAdapter<customlayout>(getContext, CustomLayoutId, custom_item) {
 
+    var nonMineTiles : Int = 0
+    var stopCountingTiles : Boolean = false
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var row = convertView
         val Holder : ViewHolder
+        var mineCounter : Int = 0
+
 
         if(row == null) {
 
+            if(stopCountingTiles == false){
+                for(i in 0..24){
+                    if(custom_item[i].cell.mineAllowed == false){
+                        nonMineTiles++
+                    }
+                }
+                stopCountingTiles = true
+            }
+
+
             var inflater = (getContext as Activity).layoutInflater
             row = inflater!!.inflate(CustomLayoutId, parent, false)
-            row.img.setOnClickListener {
-                Snackbar.make(it, "Pos = " + position + " , X = " + custom_item[position].cell.x + " , Y = " + custom_item[position].cell.y
-                    + ", MineAllowed = " + custom_item[position].cell.mineAllowed, Snackbar.LENGTH_LONG).show()
-            }
+
+                row.img.setOnClickListener {
+
+
+
+                    if(MainActivity.flagIsAllowed == false && MainActivity.gameDone == false){
+
+                        if(custom_item[position].cell.mineAllowed == true){ // If mine is clicked
+                            Snackbar.make(it, "BOOM!!!!", Snackbar.LENGTH_LONG).show()
+                            row.img.setImageResource(R.drawable.mine)
+                            MainActivity.gameDone = true // Stop the game
+                            mineCounter = 0
+                            MainActivity.restartButton!!.setBackgroundColor(Color.RED)
+                        }else if(custom_item[position].cell.mineAllowed == false && custom_item[position].cell.opened == false){ // If mine is not clicked
+                            custom_item[position].cell.opened = true
+                            nonMineTiles--
+
+                            var x_left : Int = custom_item[position].cell.x - 1
+                            var x_right : Int = custom_item[position].cell.x + 1
+                            var y_down : Int = custom_item[position].cell.y - 1
+                            var y_up : Int = custom_item[position].cell.y + 1
+
+                            // LEFT: - (1,0)
+                            if(x_left > 0){
+                                var x_cord = x_left
+                                var y_cord = custom_item[position].cell.y
+                                for(i in 0..24){
+                                    if(custom_item[i].cell.x == x_cord && custom_item[i].cell.y == y_cord && custom_item[i].cell.opened == false){
+                                        if(custom_item[i].cell.mineAllowed == true){
+                                            mineCounter++
+                                            break
+                                        }
+                                    }
+                                }
+                            }
+                            // RIGHT: + (1,0)
+                            if(x_right <= 4){
+                                var x_cord = x_right
+                                var y_cord = custom_item[position].cell.y
+                                for(i in 0..24){
+                                    if(custom_item[i].cell.x == x_cord && custom_item[i].cell.y == y_cord && custom_item[i].cell.opened == false){
+                                        if(custom_item[i].cell.mineAllowed == true){
+                                            mineCounter++
+                                            break
+                                        }
+                                    }
+                                }
+                            }
+                            // DOWN: - (0,1)
+                            if(y_down > 0){
+                                var x_cord = custom_item[position].cell.x
+                                var y_cord = y_down
+                                for(i in 0..24){
+                                    if(custom_item[i].cell.x == x_cord && custom_item[i].cell.y == y_cord && custom_item[i].cell.opened == false){
+                                        if(custom_item[i].cell.mineAllowed == true){
+                                            mineCounter++
+                                            break
+                                        }
+                                    }
+                                }
+                            }
+                            // UP: + (0,1)
+                            if(y_up <= 4){
+                                var x_cord = custom_item[position].cell.x
+                                var y_cord = y_up
+                                for(i in 0..24){
+                                    if(custom_item[i].cell.x == x_cord && custom_item[i].cell.y == y_cord && custom_item[i].cell.opened == false){
+                                        if(custom_item[i].cell.mineAllowed == true){
+                                            mineCounter++
+                                            break
+                                        }
+                                    }
+                                }
+                            }
+
+
+                            // Show how many mines are nearby
+                            if(mineCounter == 0){
+                                row.img.setImageResource(R.drawable.pizza)
+                            }else if(mineCounter == 1){
+                                row.img.setImageResource(R.drawable.one)
+                            }else if(mineCounter == 2){
+                                row.img.setImageResource(R.drawable.two)
+                            }else if(mineCounter == 3){
+                                row.img.setImageResource(R.drawable.three)
+                            }
+
+                            mineCounter = 0
+                        }
+                    }else if(MainActivity.flagIsAllowed == true && MainActivity.gameDone == false){
+                        if(custom_item[position].cell.mineAllowed == false){ // If mine is clicked
+                            MainActivity.restartButton!!.setBackgroundColor(Color.RED)
+                            Snackbar.make(it, "BOOM!!!!", Snackbar.LENGTH_LONG).show()
+                            MainActivity.gameDone = true // Stop the game
+                        }else{
+                            row.img.setImageResource(R.drawable.flag)
+                        }
+                    }
+
+                    // If total number of non mine tiles are opened , then player wins
+                    if(nonMineTiles == 0){
+                        MainActivity.gameDone = true
+                        Snackbar.make(it, "Murad Shahsuvarov IQULTE", Snackbar.LENGTH_LONG).show()
+                    }
+
+                }
 
             Holder = ViewHolder()
             Holder.img = row!!.findViewById(R.id.img) as ImageView
